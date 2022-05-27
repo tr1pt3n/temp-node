9f0c8ec1e018dfcd102c2d1ae8a07e16b3c0ef7a
tr1pt3n <nguyenhoangtuan069@gmail.com>
2022-05-27 21:47:56 +0700

    second

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //npm i <packageName> -> local
 //npm i -g <packageName> -> global
+//npm uninstall <packageName>
 //npm init 
 
 
@@ -9,3 +10,4 @@ const _ = require('lodash');
 const items = [1,[2,[3,4]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log('hello people');
